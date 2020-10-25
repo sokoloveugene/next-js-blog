de761d2cafa21e1a4795863b638c2001159872b8
Andrew <andrew@mindnow.io>
2020-10-25 17:43:40 +0200

    frontend: fix method of fetch

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -15,7 +15,7 @@ const PostWithId: React.FC<PostWithIdProps> = ({ data, requiredComments }) => {
   const { query } = useRouter();
   const { postId } = query;
   const [comments, setComments] = useState(requiredComments.reverse());
-
+  
   const handleCreate = (body) => {
     Axios.post("https://simple-blog-api.crew.red/comments", {
       postId,
@@ -37,17 +37,55 @@ const PostWithId: React.FC<PostWithIdProps> = ({ data, requiredComments }) => {
 
 export default PostWithId;
 
-export async function getServerSideProps(context) {
+// export async function getServerSideProps(context) {
+//   const res = await Axios.get(
+//     `https://simple-blog-api.crew.red/posts/${context.params.postId}?_embed=comments`
+//   );
+//   const data = res?.data;
+
+// const commentsResponse = Axios.get(
+//   "https://simple-blog-api.crew.red/comments"
+// );
+// const requiredComments = (await commentsResponse).data
+//   ?.reverse()
+//   .filter((item) => item.postId === context.query.postId);
+//   return { props: { data, requiredComments } };
+// }
+
+export async function getStaticProps(context) {
   const res = await Axios.get(
     `https://simple-blog-api.crew.red/posts/${context.params.postId}?_embed=comments`
   );
-  const data = res?.data;
+  const data = await res.data;
 
-  const commentsResponse = Axios.get(
+  const commentsResponse = await Axios.get(
     "https://simple-blog-api.crew.red/comments"
   );
+
   const requiredComments = (await commentsResponse).data
     ?.reverse()
-    .filter((item) => item.postId === context.query.postId);
-  return { props: { data, requiredComments } };
+    .filter((item) => item.postId === context.params.postId);
+
+  return {
+    props: {
+      data,
+      requiredComments,
+    },
+    revalidate: 1,
+  };
+}
+
+export async function getStaticPaths() {
+  const res = await Axios.get(
+    `https://simple-blog-api.crew.red/posts/?_embed=comments`
+  );
+  const posts = await res.data;
+
+  const paths = posts.map((post) => ({
+    params: { postId: post.id.toString() },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
 }
